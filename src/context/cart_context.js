8ae3b09472fd237df1a034eb381042b0cffa23da
Dhanur <71970682+dhanur350@users.noreply.github.com>
2023-07-23 16:46:51 +0000

    script

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -14,8 +14,8 @@ const getLocalCartData = () => {
 };
 
 const initialState = {
-  // cart: [],
-  cart: getLocalCartData(),
+  cart: [],
+  // cart: getLocalCartData(),
   total_item: "",
   total_price: "",
   shipping_fee: 50000,
